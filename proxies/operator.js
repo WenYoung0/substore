145d7c3b8a6021f1fc5f18460c9d01a1bdd72857
duakc <young@qeee.net>
2026-04-07 01:04:07 +0800

    feat & fix: add udp and return proxy correctly

--- proxies/operator.js
+++ proxies/operator.js
@@ -1,5 +1,7 @@
 function operator(proxies, targetPlatform, context) {
   return proxies.map((proxy) => {
     if (proxy.type === "anytls") proxy["min-idle-session"] = 32;
+    if (!proxy.udp) proxy.udp = true; // enable udp for all proxy
+    return proxy;
   });
 }
