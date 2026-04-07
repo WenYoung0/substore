bd9b1b8b14039acf5892cd91fa7c644ba01d659e
duakc <young@qeee.net>
2026-04-07 12:29:48 +0800

    chore(cores/box): opt dns route

---
+++ internal/cores/box/README.md
@@ -0,0 +1,5 @@
+# Warning
+
+On macos platform client (SFM), you should turn on the Setting
+`Packet Tunnel -> includeAllNetworks `
+to make sure that all dns query are hijacked by Sing-box
--- proxies/operator.js
+++ proxies/operator.js
@@ -1,7 +1,8 @@
 function operator(proxies, targetPlatform, context) {
   return proxies.map((proxy) => {
     if (proxy.type === "anytls") proxy["min-idle-session"] = 32;
-    if (!proxy.udp) proxy.udp = true; // enable udp for all proxy
+
+    proxy.udp = true; // enable udp for all proxy
     return proxy;
   });
 }
